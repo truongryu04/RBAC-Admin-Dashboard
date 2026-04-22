ed40034845268b8bc042a4d8290f93ea5448f7cf
Ryu <nguyenductruong200604@gmail.com>
2026-04-22 16:53:53 +0700

    Cấu hình lại entity role và user

--- rbac_admin_be/src/modules/role/entities/role.entity.ts
+++ rbac_admin_be/src/modules/role/entities/role.entity.ts
@@ -1,9 +1,11 @@
+import { UserRole } from 'src/modules/user/entities/user-role.entity';
 import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
   CreateDateColumn,
+  OneToMany,
 } from 'typeorm';
 @Entity('roles')
 export class Role {
@@ -12,6 +14,16 @@ export class Role {
 
   @Column({ unique: true })
   name!: string;
+
   @Column()
   description?: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  @OneToMany(() => UserRole, (ur) => ur.role)
+  userRoles!: UserRole[];
 }
--- rbac_admin_be/src/modules/user/entities/user.entity.ts
+++ rbac_admin_be/src/modules/user/entities/user.entity.ts
@@ -1,16 +1,20 @@
+import { Role } from 'src/modules/role/entities/role.entity';
 import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
   CreateDateColumn,
+  JoinTable,
+  OneToMany,
 } from 'typeorm';
+import { UserRole } from './user-role.entity';
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({ unique: true })
+  @Column()
   username!: string;
 
   @Column({ select: false })
@@ -19,11 +23,17 @@ export class User {
   @Column({ unique: true })
   email!: string;
 
-  @Column()
+  @Column({ default: 'ACTIVE' })
   status!: string;
+
+  @Column({ default: false })
+  deleted!: boolean;
   @CreateDateColumn()
   createdAt!: Date;
 
   @UpdateDateColumn()
   updatedAt!: Date;
+
+  @OneToMany(() => UserRole, (ur) => ur.user)
+  userRoles!: UserRole[];
 }
