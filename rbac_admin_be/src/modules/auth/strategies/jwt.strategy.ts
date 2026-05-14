d19ba57345927ade56df2316a5011e7829b33fc9
Ryu <nguyenductruong200604@gmail.com>
2026-05-14 13:12:39 +0700

    Sửa jwt strategy

--- rbac_admin_be/src/modules/auth/strategies/jwt.strategy.ts
+++ rbac_admin_be/src/modules/auth/strategies/jwt.strategy.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
 import { ConfigService } from '@nestjs/config';
+
 import { PassportStrategy } from '@nestjs/passport';
+
 import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { UserService } from 'src/modules/user/services/user.service';
 
 type JwtPayload = {
   sub: number;
@@ -11,19 +16,35 @@ type JwtPayload = {
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+
       ignoreExpiration: false,
+
       secretOrKey: configService.getOrThrow<string>('JWT_ACCESS_SECRET'),
     });
   }
 
   async validate(payload: JwtPayload) {
+    const user = await this.userService.findByEmail(payload.email);
+
+    if (!user) {
+      throw new UnauthorizedException('Người dùng không tồn tại');
+    }
+
     return {
-      id: payload.sub,
-      email: payload.email,
-      role: payload.role,
+      id: user.id,
+
+      email: user.email,
+
+      role: user.role?.name,
+
+      permissions:
+        user.role?.permissions?.map((permission) => permission.code) || [],
     };
   }
 }
